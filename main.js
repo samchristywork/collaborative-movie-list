07a38ce1430968d9ab5e1ddcd759a7d16725094f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-06-02 20:28:32 -0500

    Initial commit of these project files.
    This is the initial commit of the source files for this project.
    `main.js` is the main server code, with `package.json` for configuration
    management.
    
    The static files in `public` are all of the resources that get
    downloaded to the browser.

---
+++ main.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const morgan = require('morgan')
+const fs = require('fs')
+const app = express()
+
+/*
+ * Placeholder data. Each element in this array has a name and a boolean flag
+ * signaling whether it has been watched. This is the main state holder of the
+ * server.
+ */
+const movieList = {
+  list: [
+    { name: 'Movie A', watched: false },
+    { name: 'Movie B', watched: true },
+    { name: 'Movie C', watched: false }
+  ]
+}
+
+/*
+ * 'morgan' is used to log the HTTP requests that this server gets. When used
+ * this way, it will log to both a file and standard output.
+ */
+app.use(morgan('common', {
+  stream: fs.createWriteStream('./access.log', { flags: 'a' })
+}))
+app.use(morgan('dev'))
+
+/*
+ * Create a static route. Everything in the 'public' directory will be sent
+ * as-is. That includes the index, CSS styling, script files, and images.
+ */
+app.use(express.static('public'))
+
+/*
+ * This route takes a movie name and adds it to the list with the 'watched'
+ * flag set to 'false'.
+ *
+ * ex: '/add_movie?movie=Terminator%202' will add "Terminator 2" to the list.
+ *
+ * Returns '200' if the insertion was successful, otherwise '400'.
+ */
+app.get('/add_movie', function (req, res) {
+  if (req.query.movie && typeof (req.query.movie === 'string')) {
+    movieList.list.push({ name: req.query.movie, watched: false })
+    res.sendStatus(200)
+  } else {
+    res.sendStatus(400)
+  }
+})
+
+/*
+ * This route takes a movie name and removes it from the list if it exists.
+ * Note that this route can only be accessed from localhost, and does not have
+ * a UI control. For administration only.
+ *
+ * ex: '/delete?movie=Terminator%202' will delete "Terminator 2" from the list.
+ *
+ * Returns '200' if the user supplied a valid movie name, otherwise '400'.
+ */
+app.get('/delete', function (req, res) {
+  if (req.ip === '::ffff:127.0.0.1') {
+    if (req.query.movie && typeof (req.query.movie === 'string')) {
+      movieList.list = movieList.list.filter(e => {
+        return e.name !== req.query.movie
+      })
+      res.sendStatus(200)
+      return
+    }
+  }
+  res.sendStatus(400)
+})
+
+/*
+ * This route takes a movie name and toggles its 'watched' flag.
+ *
+ * ex: '/watched?movie=Terminator%202'
+ *
+ * Returns '200' if the user supplied a valid movie name, and it was in the
+ * list, otherwise '400'.
+ */
+app.get('/watched', function (req, res) {
+  if (req.query.movie && typeof (req.query.movie === 'string')) {
+    for (const movie of movieList.list) {
+      if (movie.name === req.query.movie) {
+        movie.watched = !movie.watched
+        res.sendStatus(200)
+        return
+      }
+    }
+  }
+  res.sendStatus(400)
+})
+
+/*
+ * This route simply returns the movie list.
+ *
+ * ex: '/movies.json'
+ *
+ * Returns '200' if the user supplied a valid movie name, and it was in the
+ * list, otherwise '400'.
+ */
+app.get('/movies.json', function (req, res) {
+  res.json(movieList)
+})
+
+app.listen(8080)
+console.log('Listening on port 8080...')
---
+++ public/script.js
@@ -0,0 +1,45 @@
+const movieList = document.querySelector('#movie-list')
+
+// Repopulate the movie list with an up to date version.
+function refresh () {
+  fetch('/movies.json')
+    .then(x => x.json())
+    .then(y => {
+      y.list.sort((a, b) => {
+        return a.name > b.name
+      })
+      movieList.innerHTML = ''
+      for (const movie of y.list) {
+        const d = document.createElement('div')
+        d.innerText = movie.name
+        d.classList = 'list-item'
+        if (movie.watched) {
+          d.style.textDecoration = 'line-through'
+        }
+        d.onclick = e => {
+          console.log(movie)
+          fetch(`/watched?movie=${movie.name}`)
+            .then(x => x.text())
+            .then(y => {
+              refresh()
+            })
+        }
+        movieList.appendChild(d)
+      }
+    })
+}
+
+// Add a movie to the list.
+function addMovie () {
+  if (confirm('Would you like to add a movie?')) {
+    const movie = prompt('What movie do you want to add?')
+    console.log(movie)
+    fetch(`/add_movie?movie=${movie}`)
+      .then(x => x.text())
+      .then(y => {
+        refresh()
+      })
+  }
+}
+
+refresh()
