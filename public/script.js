47eacb54dc5666df9514026fd87057f1b0477a20
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-07-11 10:39:04 -0500

    Update the client to use the new routes.
    Because the server side routes have changed, the client needs to change
    as well.

--- public/script.js
+++ public/script.js
@@ -4,23 +4,23 @@ let prefix="/collaborative-movie-list/";
 
 // Repopulate the movie list with an up to date version.
 function refresh () {
-  fetch(prefix + 'list')
+  fetch(prefix + 'list?list=movies')
     .then(x => x.json())
     .then(y => {
-      y.list.sort((a, b) => {
+      y.sort((a, b) => {
         return a.name > b.name
       })
       movieList.innerHTML = ''
-      for (const movie of y.list) {
+      for (const movie of y) {
         const d = document.createElement('div')
         d.innerText = movie.name
         d.classList = 'list-item'
-        if (movie.watched) {
+        if (movie.tagged) {
           d.style.textDecoration = 'line-through'
         }
         d.onclick = e => {
           console.log(movie)
-          fetch(prefix + `toggle?movie=${movie.name}`)
+          fetch(prefix + `toggle?list=movies&name=${movie.name}`)
             .then(x => x.text())
             .then(y => {
               refresh()
@@ -36,7 +36,7 @@ function addMovie () {
   if (confirm('Would you like to add a movie?')) {
     const movie = prompt('What movie do you want to add?')
     console.log(movie)
-    fetch(prefix + `add?movie=${movie}`)
+    fetch(prefix + `add?list=movies&name=${movie}`)
       .then(x => x.text())
       .then(y => {
         refresh()
